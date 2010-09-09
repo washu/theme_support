41d5cd16fefdeb87f3df4d8be3ed07077ccaf9ec
riven <s.claudel@tele2.fr>
2010-09-09 09:50:01 +0200

    theme :mymethod works

---
+++ lib/theme_support/patches/actioncontroller_ex.rb
@@ -0,0 +1,14 @@
+# Load the theme to execution and not in loading app
+# TODO Check if this code works this action mailer
+module ActionController
+  module ImplicitRender
+    alias_method :theme_support_default_render, :default_render
+
+    def default_render
+      theme = current_theme
+      theme = ActionView::Base.process_view_paths(File.join("themes", theme, "views"))
+      prepend_view_path(theme)
+      theme_support_default_render
+    end
+  end
+end
